79232b3c84299d4a4013a419df7429d280e352ae
iqbaladinur <iqbaladinur@gmail.com>
2017-11-16 18:11:16 +0700

    change readme

--- application/config/database.php
+++ application/config/database.php
@@ -48,9 +48,9 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['hostname'] = '127.0.0.1';
+$db['default']['username'] = 'azure';
+$db['default']['password'] = '6#vWHD_$';
 $db['default']['database'] = 'kateringdb';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
