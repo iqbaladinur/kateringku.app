5ae27a9e4f1c631aab861c789e5dac69d022d76a
iqbaladinur <iqbaladinur@gmail.com>
2017-11-16 18:18:20 +0700

    change dbconfig

--- application/config/database.php
+++ application/config/database.php
@@ -48,7 +48,7 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = '127.0.0.1';
+$db['default']['hostname'] = '127.0.0.1:54324';
 $db['default']['username'] = 'azure';
 $db['default']['password'] = '6#vWHD_$';
 $db['default']['database'] = 'kateringdb';
